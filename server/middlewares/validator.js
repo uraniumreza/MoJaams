558758bdbdb67cb0b3ea4a90ebc267d98e3df193
uraniumreza <reza.uranium@gmail.com>
2019-12-28 14:02:17 +0600

    Add query and params support along with body to the validator

--- server/middlewares/validator.js
+++ server/middlewares/validator.js
@@ -1,19 +1,20 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const validator = (schema, property) => {
   return (req, res, next) => {
-  const { error } = Joi.validate(req.body, schema);
-  const valid = error == null;
+    const { error } = Joi.validate(req[property], schema);
+    const valid = error == null;
 
-  if (valid) {
-    next();
-  } else {
-    const { details } = error;
-    const message = details.map(i => i.message).join(',');
+    if (valid) {
+      next();
+    } else {
+      const { details } = error;
+      const message = details.map(i => i.message).join(",");
 
-    console.log("error", message);
-   res.status(422).json({ error: message }) }
-  }
-}
+      console.log("error", message);
+      res.status(422).json({ error: message });
+    }
+  };
+};
 
 module.exports = validator;
