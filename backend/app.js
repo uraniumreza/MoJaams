93036df3399ce243f88cb89c3cb11edc81c73e85
uraniumreza <reza.uranium@gmail.com>
2019-12-31 08:42:36 +0600

    Add CORS

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,21 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+
+  next();
+});
+
 // Setup routes
 app.use('/api', routes);
 
