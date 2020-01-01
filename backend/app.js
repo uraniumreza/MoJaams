2f92cd80411ce34901f19cac322663d03cc25ae7
uraniumreza <reza.uranium@gmail.com>
2020-01-01 22:02:35 +0600

    Remove redundant fallback route

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,10 @@ const bodyParser = require('body-parser');
 const { handleError } = require('./src/services/error');
 const routes = require('./src/routes');
 
-// Set up the express app
 const app = express();
 
-// TODO: Use Morgan for logging!
-// Log requests to the console.
 app.use(logger('dev'));
 
-// Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -30,16 +26,8 @@ app.use((req, res, next) => {
   next();
 });
 
-// Setup routes
 app.use('/api', routes);
 
-app.use((req, res, next) => {
-  const error = new Error('Not found');
-  error.status = 404;
-  next(error);
-});
-
-// Error handling
 app.use((error, req, res) => {
   handleError(error, res);
 });
