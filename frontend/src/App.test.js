8dd8c22661145dba420c609f03b6788f5d353266
uraniumreza <reza.uranium@gmail.com>
2020-01-01 17:09:25 +0600

    Fix front-end test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = getByText(/Welcome to MoJaams/i);
+  expect(element).toBeInTheDocument();
 });
