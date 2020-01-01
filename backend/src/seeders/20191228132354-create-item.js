dd0217e1d9115a7d1e8330d32de4d27b9352c2fe
uraniumreza <reza.uranium@gmail.com>
2020-01-02 02:57:55 +0600

    Update seeds

--- backend/src/seeders/20191228132354-create-item.js
+++ backend/src/seeders/20191228132354-create-item.js
@@ -5,16 +5,19 @@ module.exports = {
       [
         {
           name: 'Margarita',
+          status: 'active',
           createdAt: Sequelize.literal('NOW()'),
           updatedAt: Sequelize.literal('NOW()'),
         },
         {
           name: 'Four Seasons',
+          status: 'active',
           createdAt: Sequelize.literal('NOW()'),
           updatedAt: Sequelize.literal('NOW()'),
         },
         {
           name: 'Meat Lovers',
+          status: 'inactive',
           createdAt: Sequelize.literal('NOW()'),
           updatedAt: Sequelize.literal('NOW()'),
         },
@@ -24,6 +27,10 @@ module.exports = {
 
   down: (queryInterface, Sequelize) => {
     const { Op } = Sequelize;
-    return queryInterface.bulkDelete('Items', { id: { [Op.in]: [1, 2] } }, {});
+    return queryInterface.bulkDelete(
+      'Items',
+      { id: { [Op.in]: [1, 2, 3] } },
+      {},
+    );
   },
 };
